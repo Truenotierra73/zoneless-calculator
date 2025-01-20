342b07fe8e7e66ef7071e130b3d9335df5b21e4b
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-01-20 12:18:56 -0300

    test(calculator-view): agregar test unitarios

--- src/app/calculator/views/calculator-view/calculator-view.component.spec.ts
+++ src/app/calculator/views/calculator-view/calculator-view.component.spec.ts
@@ -5,19 +5,39 @@ import CalculatorViewComponent from './calculator-view.component';
 describe('CalculatorViewComponent', () => {
   let component: CalculatorViewComponent;
   let fixture: ComponentFixture<CalculatorViewComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CalculatorViewComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CalculatorViewComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create CalculatorViewComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain "calculator" component', () => {
+    const calculatorEl: HTMLElement | null = compiled.querySelector('calculator');
+    expect(calculatorEl).not.toBeNull();
+  });
+
+  it('should contain "div" element and basic css classes', () => {
+    const divEl: HTMLElement | null = compiled.querySelector('div');
+
+    expect(divEl).not.toBeNull();
+
+    const mustHaveClasses: string[] = 'w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden'.split(' ');
+    const divClasses: string[] = divEl!.classList.value.split(' ');
+
+    mustHaveClasses.forEach(classname => {
+      expect(divClasses).toContain(classname);
+    });
   });
 });
